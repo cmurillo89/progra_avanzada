24bf686f07e7763e43770aefc224b3082935b79e
Cristhofer Murillo Gómez <94995725+cmurillo89@users.noreply.github.com>
2024-06-04 21:09:21 -0600

    terminamos de agregar las vistas y templates necesarios para el CRUD de peliculas, ahora vamos a agregar las vistas y templates necesarios para el CRUD de actores

--- sistema/peliculas/urls.py
+++ sistema/peliculas/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('soporte/', views.soporte, name='soporte'),
+    path('index/', views.index, name='index'),
+    path('agregar/', views.agregar, name='agregar'),
+    path('editar/', views.editar, name='editar'),
 ]
--- sistema/peliculas/views.py
+++ sistema/peliculas/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
+def soporte(request):
+    return render(request, 'paginas/soporte.html')
+
+def index(request):
+    return render(request, 'peliculas/index.html')
+
+def agregar(request):
+    return render(request, 'peliculas/agregar.html')
+
+def editar(request):
+    return render(request, 'peliculas/editar.html')
